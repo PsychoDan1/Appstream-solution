60d865731edb85ec3a888706d7f8abad5da59ea7
root <root@vagrant.net>
2022-07-16 22:10:05 +0300

    vars.py

--- appstream_solution/appstream_solution_stack.py
+++ appstream_solution/appstream_solution_stack.py
@@ -3,6 +3,7 @@ import aws_cdk as cdk
 import os
 from constructs import Construct
 import aws_cdk.aws_appstream as appstream
+from appstream_solution import vars
 
 class AppstreamSolutionStack(Stack):
 
@@ -10,7 +11,7 @@ class AppstreamSolutionStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         cfn_fleet = appstream.CfnFleet(self, "AppstreamNonProd-Phoenix-solution-fleet",
-            instance_type="stream.standard.large",
+            instance_type = vars.instanceType,
             name="AppstreamNonProd-Phoenix-solution-fleet",
             compute_capacity = appstream.CfnFleet.ComputeCapacityProperty(
                 desired_instances = 20
@@ -20,16 +21,17 @@ class AppstreamSolutionStack(Stack):
             idle_disconnect_timeout_in_seconds = 960,
             enable_default_internet_access = False,
             fleet_type = "ALWAYS_ON",
-            image_name = "LinuxOSv4_AppStream",
+            image_name = vars.imageName,
             max_user_duration_in_seconds = 57600,
-            stream_view = "DESKTOP",
+            stream_view = vars.streamView,
             tags = [cdk.CfnTag(
                 key = "Name",
                 value = "AppstreamNonProd-Phoenix-solution"
             )],
             vpc_config = appstream.CfnFleet.VpcConfigProperty(
                 security_group_ids = ["sg-015ddf2746c8025a2"],
-                subnet_ids = ["subnet-0b6b0d8d84785308e","subnet-0fb64d475f117c104"]
+                subnet_ids = vars.subnets
             )
 
         )
+
