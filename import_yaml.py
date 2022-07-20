6d823b6cca50fbf58d81d0592c5b633a12387e99
System Administrator <root@omris-MacBook-Air.local>
2022-07-20 13:46:01 +0300

    initial commit

--- import_yaml.py
+++ import_yaml.py
@@ -5,9 +5,9 @@ import yaml
 with open('manifest.yml', 'rb') as f:
         config = yaml.safe_load(f)
 
-core_config = config['spec']
-env = core_config['environments']
-env_name = env['name']
-#print(core_config)
-print(env_name)
+core_config = config['environment']
+subnet_config = core_config['streamView']
+print(subnet_config)
+
+
 
